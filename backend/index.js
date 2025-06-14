a2c16794b70c2cc8aad42e9861f89c48fc2d6b59
ibharathboga <ibharathboga@gmail.com>
2025-06-14 15:57:05 +0530

    vercel-trial:update:backend/index.js

--- backend/index.js
+++ backend/index.js
@@ -46,3 +46,5 @@ mongoose.connect(process.env.DB_URL)
   .catch(err => console.error('MongoDB connection error:', err));
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+module.exports = app;
